b6a2aef7dbac1b88305e6705558ee2832656e461
Patrick A Wallace <pawrsccouk@googlemail.com>
2016-01-17 18:14:25 +0000

    Dates in the XML file now use a standard format which is independent of the user's locale and time/time settings.
    This fixes a bug where loading XML files after a locale change causes a crash as NSDateFormatter cannot parse the date strings.

--- CharSheet-Swift/LogEntry.swift
+++ CharSheet-Swift/LogEntry.swift
@@ -26,11 +26,26 @@ class LogEntry : NSManagedObject
     }
 }
 
-private var fullDateFormatter: NSDateFormatter = {
-	let cachedFormatter = NSDateFormatter()
-	cachedFormatter.dateStyle = .FullStyle
-	cachedFormatter.timeStyle = .FullStyle
-	return cachedFormatter
+/// A date formatter designed to output dates to XML files 
+///
+/// This outputs dates in a format which will not change even if the user's locale does.
+private var xmlDateFormatter: NSDateFormatter = {
+	let dateFormatter = NSDateFormatter()
+	// en_US_POSIX is a locale which gives standard US-like date and time formats and which is guaranteed never to change.
+	dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+	dateFormatter.dateFormat = "yyyy'-'MM'-'dd' 'HH':'mm':'ss"
+	return dateFormatter
+}()
+
+/// This was the old date formatter, kept for backwards compatibility.
+///
+/// - note: Do not write dates with this, it is just for reading old formats.
+///         Use **xmlDateFormatter** instead.
+private var backwardsCompatibleDateFormatter: NSDateFormatter = {
+	let dateFormatter = NSDateFormatter()
+	dateFormatter.dateStyle = .FullStyle
+	dateFormatter.timeStyle = .FullStyle
+	return dateFormatter
 }()
 
 // XML entity and attribute tags for this object.
@@ -47,7 +62,7 @@ extension LogEntry: XMLClient
 			return DDXMLNode.attributeWithName(name.rawValue, stringValue: value) as! DDXMLNode
 		}
         let this = DDXMLElement.elementWithName(LOG_ENTRY, stringValue:self.change) as! DDXMLElement
-        this.addAttribute( attribute(.DATE_TIME, value: fullDateFormatter.stringFromDate(self.dateTime)) )
+        this.addAttribute( attribute(.DATE_TIME, value: xmlDateFormatter.stringFromDate(self.dateTime)) )
         this.addAttribute( attribute(.SUMMARY  , value: self.summary!) )
         return this;
     }
@@ -56,11 +71,29 @@ extension LogEntry: XMLClient
     
     func updateFromXML(element: DDXMLElement) throws
 	{
+		/// Attempt to parse a date using multiple date formatters for backwards compatibility.
+		/// 
+		/// This first tries to parse the date using the 'standard' xml date formatter provided.
+		/// If this fails, it tries to use the default date formatter, which was how we used to parse dates.
+		/// This allows me to update the format while still being able to load in old files.
+		func parseDate(dateText: String) throws -> NSDate
+		{
+			var date = xmlDateFormatter.dateFromString(dateText)
+			if date == nil {
+				date = backwardsCompatibleDateFormatter.dateFromString(dateText)
+			}
+			guard let d = date else {
+				throw XMLSupport.XMLError("Error converting \(dateText) into a date.")
+			}
+			return d
+		}
+
+
         try XMLSupport.validateElementName(element.name, expectedName: LOG_ENTRY)
         for attrNode in (element.attributes as! [DDXMLNode]) {
             if let nodeName = Attribute(rawValue: attrNode.name) {
                 switch nodeName {
-                case .DATE_TIME: self.dateTime = fullDateFormatter.dateFromString(attrNode.stringValue)!
+                case .DATE_TIME: self.dateTime = try parseDate(attrNode.stringValue)
                 case .SUMMARY:   self.summary  = attrNode.stringValue
                 }
             }
--- Notes.md
+++ Notes.md
@@ -10,11 +10,12 @@
 [x] Find out how Steppers & Input Fields are supposted to work.
 
 ## Bugs
+[ ] When importing a character the date is always added, even if there is no character with that name already.
 [ ] When rearranging specialties in the Edit view, the old skill cell in the Use view doesn't update.
 	Open a character, pick any skill. Note the positions in the 'use' cell. Then go Edit, Skill Info, drag the specialties.
 	Close skill info, close main edit page. Note the specialties have not been updated in the 'Use' view.
 [x] Disable the Edit, Notes, XP etc. in the Use Controller when there are no characters selected in the table view controller.
-[ ] Dates in the XML files use the locale-dependent full format, which means you can get crashes reading back older XML files.
+[x] Dates in the XML files use the locale-dependent full format, which means you can get crashes reading back older XML files.
     We should use a canonical format, such as YYYY-MM-DD HH:MM which has no ambiguity.
 	(We can try and parse the full format as well, for backwards compatibility but only if the canonical format isn't found)
 [x] ExtraD4 initial value should default to 1 or be taken from the die roll object.
